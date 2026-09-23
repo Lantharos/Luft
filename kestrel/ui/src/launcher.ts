f0920dde4de3be8c8dc236f7e7d9a67041c58a35
Kristof Imeri <kristof@lanth.me>
2026-09-24 00:27:44 +0200

    Restore subtle unscaled separation in Luft launcher hover

--- kestrel/ui/src/launcher.ts
+++ kestrel/ui/src/launcher.ts
@@ -1,20 +1,35 @@
+import Clutter from 'gi://Clutter';
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import St from 'gi://St';
 
-import luft from './assets/luft.svg';
-import { liftIcon } from './motion.js';
+import primary from './assets/luft-primary.svg';
+import secondary from './assets/luft-secondary.svg';
+import { animateActor } from './motion.js';
 
 export function createLauncher(activate: () => void): St.Button {
-  const icon = new St.Icon({
-    gicon: Gio.BytesIcon.new(new GLib.Bytes(new TextEncoder().encode(luft))),
-    icon_size: 24,
-  });
+  const mark = new St.Widget({ width: 24, height: 24, layout_manager: new Clutter.BinLayout() });
   const button = new St.Button({
-    style_class: 'kestrel-task-button', child: icon,
+    style_class: 'kestrel-task-button', child: mark,
     width: 40, height: 40, can_focus: true, track_hover: true, accessible_name: 'Start',
   });
-  liftIcon(button, icon);
+  for (const [svg, direction] of [[secondary, 1], [primary, -1]] as const) {
+    const icon = new St.Icon({
+      gicon: Gio.BytesIcon.new(new GLib.Bytes(new TextEncoder().encode(svg))),
+      icon_size: 24,
+    });
+    mark.add_child(icon);
+    const update = () => {
+      const offset = button.hover && !button.pressed ? 0.25 : 0;
+      animateActor(icon, {
+        translation_x: direction * offset,
+        translation_y: direction * offset,
+        duration: 150, mode: Clutter.AnimationMode.EASE_OUT_QUAD,
+      });
+    };
+    button.connect('notify::hover', update);
+    button.connect('notify::pressed', update);
+  }
   button.connect('clicked', activate);
   return button;
 }
